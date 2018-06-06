d5ac916b0fba52d6730263e676e137fcae8f9be9
Paritosh Pawar <Mindstix@The-ParitoshPawars-MacBook-Pro.local>
2018-06-06 21:36:13 +0530

    Updated the app delegate file

--- Example/smartInteractionEngine/AppDelegate.swift
+++ Example/smartInteractionEngine/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        var obj = sampleFile()
+//        var obj = sampleFile()
 //        sampleFile().makePostCallWithAlamofire(NFCID: "12345", completionHandler: {
 //            dict in
 //            print(dict)
